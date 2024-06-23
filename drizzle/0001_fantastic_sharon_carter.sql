a49e9c7b6298b14c026c7cb33ce8e511209f94c5
HansFigo <hambasahaya0303@gmail.com>
2024-06-23 15:05:07 +0700

    Refactor likes table to drop and add foreign key constraint

--- drizzle/0001_fantastic_sharon_carter.sql
+++ drizzle/0001_fantastic_sharon_carter.sql
@@ -1,1 +1,5 @@
+ALTER TABLE `likes` DROP FOREIGN KEY `likes_user_fk`; 
+
 ALTER TABLE `likes` DROP INDEX `likes_user_id_unique`;
+
+ALTER TABLE `likes` ADD CONSTRAINT `likes_user_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
