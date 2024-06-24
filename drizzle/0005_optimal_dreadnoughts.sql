c9fc83e8217b800209edf672b030efa00f299f08
HansFigo <hambasahaya0303@gmail.com>
2024-06-25 00:01:01 +0700

    Improve User Schema

---
+++ drizzle/0005_optimal_dreadnoughts.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `user` ADD `location` varchar(256);--> statement-breakpoint
+ALTER TABLE `user` ADD `instagram` varchar(256);--> statement-breakpoint
+ALTER TABLE `user` ADD `linkedin` varchar(256);--> statement-breakpoint
+ALTER TABLE `user` ADD `behance` varchar(256);--> statement-breakpoint
+ALTER TABLE `user` ADD `headline` varchar(256);
